a1b26a50e1a9e70173797445601401bbcaa8a85a
Sondos Amr <sondos.amr444@gmail.com>
2025-03-08 16:26:00 +0200

    Add modal functionality with open/close logic and Escape key support

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+const modal = document.querySelector('.hidden');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeHidden = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    openModal();
+  });
+}
+
+closeModal.addEventListener('click', closeHidden);
+overlay.addEventListener('click', closeHidden);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape' && !modal.classList.contains('hidden')) closeHidden();
+});
